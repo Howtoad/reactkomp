fab3585859f82d3bc8453e190f652c7576c58d74
Kim <howtoad123@gmail.com>
2022-05-19 13:31:15 +0200

    done

--- src/Coloredcards.js
+++ src/Coloredcards.js
@@ -7,14 +7,29 @@ const Coloredcards = (props) => {
       display: flex;
       background-color: ${props.backgroundcolor};
       max-width: 260px;
+      border-radius: 10px;
+      display: grid;
+      padding: 20px;
+      color: white;
       & .iconcontainer {
         font-size: 36px;
+        margin-left: auto;
+      }
+      & h2 {
+        padding-top: 20px;
+      }
+      & p {
+        padding-top: 20px;
+        padding-bottom: 20px;
       }
     `,
   };
+  console.log(FaTruck);
   return (
     <div css={styles.cardcontainer}>
-      <div className="iconcontainer">{props.icon}</div>
+      <div className="iconcontainer">
+        <FaTruck />
+      </div>
       <h2>{props.title}</h2>
       <p>{props.text}</p>
     </div>
