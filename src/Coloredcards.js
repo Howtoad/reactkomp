dd30dba897b1cebfcfb0589eed3d514ef7735afe
Kim <howtoad123@gmail.com>
2022-05-19 13:18:13 +0200

    xd

--- src/Coloredcards.js
+++ src/Coloredcards.js
@@ -1,12 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-const Coloredcards = () => {
+import { FaTruck } from "react-icons/fa";
+const Coloredcards = (props) => {
   const styles = {
     cardcontainer: css`
       display: flex;
+      background-color: ${props.backgroundcolor};
+      max-width: 260px;
+      & .iconcontainer {
+        font-size: 36px;
+      }
     `,
   };
-  return <div css={styles.cardcontainer}></div>;
+  return (
+    <div css={styles.cardcontainer}>
+      <div className="iconcontainer">{props.icon}</div>
+      <h2>{props.title}</h2>
+      <p>{props.text}</p>
+    </div>
+  );
 };
 
 export default Coloredcards;
